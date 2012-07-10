bc7d0dc742f6dd29f5ac17923d1ad98cf794edca
eprogramming <00hf11@gmail.com>
2012-07-10 02:41:35 +0000

    just initial setup...

---
+++ toskeiraspace/src/main/java/com/google/code/ts/pojo/Player.java
@@ -0,0 +1,59 @@
+package com.google.code.ts.pojo;
+
+/**
+ * The Class Player.
+ * @author Leonardo Silveira
+ * @author Helio Frota
+ */
+public class Player {
+
+	/** The x. */
+	private int x;
+	
+	/** The y. */
+	private int y;
+	
+	/**
+	 * Instantiates a new player.
+	 */
+	public Player() {
+		
+	}
+
+	/**
+	 * Gets the x.
+	 *
+	 * @return the x
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * Sets the x.
+	 *
+	 * @param x the new x
+	 */
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	/**
+	 * Gets the y.
+	 *
+	 * @return the y
+	 */
+	public int getY() {
+		return y;
+	}
+
+	/**
+	 * Sets the y.
+	 *
+	 * @param y the new y
+	 */
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+}
---
+++ toskeiraspace/src/main/java/com/google/code/ts/servlet/ToskeiraSpace.java
@@ -0,0 +1,19 @@
+package com.google.code.ts.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet class ToskeiraServlet.
+ * 
+ * @author Leonardo Silveira
+ * @author Helio Frota
+ *
+ */
+@WebServlet(value="/ts", asyncSupported = false)
+@SuppressWarnings("serial")
+public class ToskeiraSpace extends HttpServlet {
+
+	
+	
+}
