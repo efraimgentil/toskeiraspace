4f65a6e332aa2c9bb0a5c523aea5b22b4da7234f
eprogramming <00hf11@gmail.com>
2012-07-10 02:51:00 +0000

    removing spaces...

--- toskeiraspace/src/main/java/com/google/code/ts/pojo/Player.java
+++ toskeiraspace/src/main/java/com/google/code/ts/pojo/Player.java
@@ -7,53 +7,53 @@ package com.google.code.ts.pojo;
  */
 public class Player {
 
-	/** The x. */
-	private int x;
-	
-	/** The y. */
-	private int y;
-	
-	/**
-	 * Instantiates a new player.
-	 */
-	public Player() {
-		
-	}
+    /** The x. */
+    private int x;
 
-	/**
-	 * Gets the x.
-	 *
-	 * @return the x
-	 */
-	public int getX() {
-		return x;
-	}
+    /** The y. */
+    private int y;
 
-	/**
-	 * Sets the x.
-	 *
-	 * @param x the new x
-	 */
-	public void setX(int x) {
-		this.x = x;
-	}
+    /**
+     * Instantiates a new player.
+     */
+    public Player() {
 
-	/**
-	 * Gets the y.
-	 *
-	 * @return the y
-	 */
-	public int getY() {
-		return y;
-	}
+    }
 
-	/**
-	 * Sets the y.
-	 *
-	 * @param y the new y
-	 */
-	public void setY(int y) {
-		this.y = y;
-	}
-	
+    /**
+     * Gets the x.
+     *
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Sets the x.
+     *
+     * @param x the new x
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * Gets the y.
+     *
+     * @return the y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Sets the y.
+     *
+     * @param y the new y
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
 }
