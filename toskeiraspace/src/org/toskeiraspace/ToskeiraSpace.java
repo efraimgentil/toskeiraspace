3372ef66aae62d8cc326a438cbc427711b329f5b
eprogramming <00hf11@gmail.com>
2012-10-15 02:31:35 +0000

    spark working with jetty "embebedado" !

--- toskeiraspace/src/org/toskeiraspace/ToskeiraSpace.java
+++ toskeiraspace/src/org/toskeiraspace/ToskeiraSpace.java
@@ -1,18 +1,21 @@
+package org.toskeiraspace;
+
 import static spark.Spark.*;
 import spark.*;
+
 
 public class ToskeiraSpace {
 
 
     public static void main(String[] args) {
 
-        get(new Route("/hello") {
+        get(new Route("/ts") {
             @Override
             public Object handle(Request request, Response response) {
-                return "Hello World!";
+                return "Bem vindo ao toskeira space !";
             }
         });
 
-    }
+     }
 
 }
