c31b40184355d9693f99383091c03bb32b7bfbe4
eprogramming <00hf11@gmail.com>
2012-10-15 05:24:33 +0000

    removed web.xml and tests with static content on spark.

--- toskeiraspace/src/org/toskeiraspace/ToskeiraSpace.java
+++ toskeiraspace/src/org/toskeiraspace/ToskeiraSpace.java
@@ -2,17 +2,28 @@ package org.toskeiraspace;
 
 import static spark.Spark.*;
 import spark.*;
-
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
 
 public class ToskeiraSpace {
 
+    private static String renderHtml(String html) {
+        try {
+            File file = new File(html);
+            return FileUtils.readFileToString(file);
+        } catch (IOException e) {
+            return "";
+        }
+    }
 
     public static void main(String[] args) {
 
-        get(new Route("/ts") {
+        get(new Route("/") {
             @Override
             public Object handle(Request request, Response response) {
-                return "Bem vindo ao toskeira space !";
+                response.type("text/html");
+                return renderHtml("web/index.html");
             }
         });
 
