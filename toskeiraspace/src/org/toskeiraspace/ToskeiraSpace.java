36016b2bc341efdb93335bad2784e6bf6be865b6
eprogramming <00hf11@gmail.com>
2012-10-20 02:12:20 +0000

    istáile shit

--- toskeiraspace/src/org/toskeiraspace/ToskeiraSpace.java
+++ toskeiraspace/src/org/toskeiraspace/ToskeiraSpace.java
@@ -1,32 +1,52 @@
 package org.toskeiraspace;
 
-import static spark.Spark.*;
-import spark.*;
-import java.io.File;
 import java.io.IOException;
-import org.apache.commons.io.FileUtils;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class ToskeiraSpace {
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.server.handler.*;
+import org.eclipse.jetty.server.Handler;
 
-    private static String renderHtml(String html) {
-        try {
-            File file = new File(html);
-            return FileUtils.readFileToString(file);
-        } catch (IOException e) {
-            return "";
-        }
+@SuppressWarnings("serial")
+public class ToskeiraSpace extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("index.html").forward(req, resp);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(4567);
+        WebAppContext webAppContext = new WebAppContext("web", "/");
+        server.setHandler(webAppContext);
 
-        get(new Route("/") {
-            @Override
-            public Object handle(Request request, Response response) {
-                response.type("text/html");
-                return renderHtml("web/index.html");
-            }
-        });
+        /*ResourceHandler rh = new ResourceHandler();
+        rh.setDirectoriesListed(true);
+        rh.setWelcomeFiles(new String[]{"index.html"});
+        rh.setResourceBase(".");
 
-     }
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] {rh, new DefaultHandler()});
+        server.setHandler(handlers);*/
 
+        /*ContextHandler context = new ContextHandler();
+        context.setContextPath("/tsctx");
+        context.setResourceBase(".");
+        context.setClassLoader(Thread.currentThread().getContextClassLoader());
+        server.setHandler(context);*/
+
+        WebAppContext context = new WebAppContext();
+        context.setDescriptor("web/WEB-INF/web.xml");
+        context.setResourceBase("web");
+        context.setContextPath("/");
+        context.setParentLoaderPriority(true);
+        server.setHandler(context);
+
+        server.start();
+        server.join();
+    }
 }
