111c571c29a8ed7c131a2b1b682b480a28e555ba
Efraim Gentil <efraim.gentil@gmail.com>
2014-04-21 18:39:52 -0300

    Diagonal shot
    1. The shot now goes in a diagonal way ( no more teleguided )
    2. The shot now check colision with other objects around it
    3. Put more speed in the bullet

--- src/main/webapp/js/ts.js
+++ src/main/webapp/js/ts.js
@@ -63,6 +63,7 @@ function Rock(p) {
         Sprite.call(this, p);
         this.x = Math.random() * 800;
         this.y = Math.random() * 600;
+        this.width = 5;
         this.points = [ {
                 x : -5 + Math.random() * -2,
                 y : 5 + Math.random() * 3
@@ -104,32 +105,37 @@ function Bullet(p) {
         Sprite.call(this, p);
         this.dx = p.dx;
         this.dy = p.dy;
-        this.range = 300;
+        this.range = 400;
         this.target = p.target;
+        this.speed = 1.7;
         this.step = function() {
-                var mx = this.dx - this.x;
-                mx = Math.sqrt(mx * mx);
-                var my = this.dy - this.y;
-                my = Math.sqrt(my * my);
-                var stopped = true;
-                if (mx > 5) {
-                        if (this.dx >= this.x) {
-                                this.x += 1.1;
-                                stopped = false;
-                        } else {
-                                this.x -= 1.1;
-                                stopped = false;
-                        }
-                }
-                if (my > 5) {
-                        if (this.dy >= this.y) {
-                                stopped = false;
-                                this.y += 1.1;
-                        } else {
-                                stopped = false;
-                                this.y -= 1.1;
-                        }
-                }
+        		
+        	this.x += this.speed * Math.cos( this.angle );
+        	this.y += this.speed * Math.sin( this.angle );
+        	
+//                var mx = this.dx - this.x;
+//                mx = Math.sqrt(mx * mx);
+//                var my = this.dy - this.y;
+//                my = Math.sqrt(my * my);
+                var stopped = false;
+//                if (mx > 5) {
+//                        if (this.dx >= this.x) {
+//                                this.x += 1.1;
+//                                stopped = false;
+//                        } else {
+//                                this.x -= 1.1;
+//                                stopped = false;
+//                        }
+//                }
+//                if (my > 5) {
+//                        if (this.dy >= this.y) {
+//                                stopped = false;
+//                                this.y += 1.1;
+//                        } else {
+//                                stopped = false;
+//                                this.y -= 1.1;
+//                        }
+//                }
                 // it will not float forever...
                 if (stopped || this.range < 0) {
                         this.isDead = true;
@@ -147,6 +153,39 @@ function Bullet(p) {
                 ctx.arc(0, 0, 4, 0, Math.PI * 2, false);
                 ctx.stroke();
         };
+        this.checkColision = function( arrObject  , ctx){
+        	
+        	var bulletColisionRangeX1 = this.x - 30;
+        	var bulletColisionRangeY1 = this.y - 30;
+        	
+        	//Show check colision area
+//        	ctx.strokeStyle = this.color;
+//            ctx.beginPath();
+//            ctx.rect( bulletColisionRangeX1 , bulletColisionRangeY1 , 60, 60);
+//            ctx.stroke();
+        	
+        	function insideX( objX ){
+        		return (bulletColisionRangeX1 <= objX && objX  <= ( bulletColisionRangeX1 + 60 ) );
+        	}
+        	
+        	function insideY( objY ){
+        		return (bulletColisionRangeY1 <= objY && objY <= ( bulletColisionRangeY1 + 60 ) );
+        	}
+        	
+        	var objLength = arrObject.length;
+        	for(var i = 0; i < objLength ; i++){
+        		var obj = arrObject[i];
+        		if( insideX( obj.x )  && insideY( obj.y ) ){
+        			//console.log("Entering in check colision area");
+        			if( ( obj.x - obj.width ) <= ( this.x + 2 ) && ( this.x - 2 ) <=  (obj.x + obj.width)
+        				 && ( obj.y - obj.width ) <= ( this.y + 2 ) && ( this.y - 2 ) <=  (obj.y + obj.width)	 ){
+//        				console.log("colided");
+        				this.isDead = true;
+        				obj.isDead = true;
+        			}
+        		}
+        	}
+        };
 }
 
 // almighty starship
@@ -157,7 +196,8 @@ function Ship(p) {
                 // rotate
                 var x = player.move.x - this.x;
                 var y = player.move.y - this.y;
-                this.angle = Math.atan2(y, x) + Math.PI / 2;
+                this.originalAngle = Math.atan2(y, x);
+                this.angle = this.originalAngle + Math.PI / 2;
                 
                 var k  = Math.abs(this.angle).toFixed(1); 
                 var w  = Math.abs(player.pangle).toFixed(1);
@@ -176,7 +216,7 @@ function Ship(p) {
                 }
                 if (this.openFire && player.target) {
                         this.openFire = false;
-                        bullets.push(makeBullet(this.x, this.y, this.angle,
+                        bullets.push(makeBullet(this.x, this.y, this.originalAngle,
                                         player.target.x, player.target.y, player.target));
                 }
         };
@@ -273,6 +313,7 @@ function draw(ctx) {
         i = bullets.length;
         while (i--) {
                 ctx.save();
+                bullets[i].checkColision( asteroids , ctx );
                 bullets[i].draw(ctx);
                 ctx.restore();
         }
